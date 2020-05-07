d426f3d091ade90c91fa1aa9d32daae7769111bc
rlandon96 <50156134+rlandon96@users.noreply.github.com>
2020-05-06 19:55:57 -0700

    Space_Shooter_01

---
+++ Space Shooter/Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour
+{
+
+    public GameObject explosion;
+    public Transform explosionPoint;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Projectile")
+        {
+            Instantiate(explosion, explosionPoint.position, transform.rotation);
+            Destroy(other.gameObject);
+            Destroy(gameObject);
+        }
+    }
+
+}
---
+++ Space Shooter/Assets/Scripts/RandomRotator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomRotator : MonoBehaviour
+{
+
+    public float tumble;
+
+    private Rigidbody rigidbody;
+
+    private void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        rigidbody.angularVelocity = Random.insideUnitSphere * tumble;
+    }
+
+}
