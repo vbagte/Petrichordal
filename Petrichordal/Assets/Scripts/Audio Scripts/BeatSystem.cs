3f48b05fd061f1fff17d714e6ba6545dde09c5b0
M@ <60533071+xblisteredavalon@users.noreply.github.com>
2020-05-19 16:53:15 -0700

    FMOD Integration
    - added FMOD Integration
    - added BeatSystem script
    - modified PlayerController script to play  level 01 music and primary fire sfx

---
+++ Petrichordal/Assets/Scripts/Audio Scripts/BeatSystem.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class BeatSystem : MonoBehaviour
+{
+    [StructLayout(LayoutKind.Sequential)]
+    class TimelineInfo
+    {
+        public int currentMusicBar = 0;
+        public FMOD.StringWrapper lastMarker = new FMOD.StringWrapper();
+    }
+
+    TimelineInfo timelineInfo;
+    GCHandle timelineHandle;
+
+    FMOD.Studio.EVENT_CALLBACK beatCallback;
+
+    public static int bar;
+    public static string marker;
+
+    public void AssignBeatEvent(FMOD.Studio.EventInstance instance)
+    {
+        timelineInfo = new TimelineInfo();
+        timelineHandle = GCHandle.Alloc(timelineInfo, GCHandleType.Pinned);
+        beatCallback = new FMOD.Studio.EVENT_CALLBACK(BeatEventCallback);
+        instance.setUserData(GCHandle.ToIntPtr(timelineHandle));
+        instance.setCallback(beatCallback, FMOD.Studio.EVENT_CALLBACK_TYPE.TIMELINE_BEAT | FMOD.Studio.EVENT_CALLBACK_TYPE.TIMELINE_MARKER);
+        
+    }
+
+  
+
+    public void StopAndClear(FMOD.Studio.EventInstance instance)
+    {
+        instance.setUserData(IntPtr.Zero);
+        instance.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+        instance.release();
+        timelineHandle.Free();
+    }
+
+    [AOT.MonoPInvokeCallback(typeof(FMOD.Studio.EVENT_CALLBACK))]
+    static FMOD.RESULT BeatEventCallback(FMOD.Studio.EVENT_CALLBACK_TYPE type, FMOD.Studio.EventInstance instance, IntPtr parameterPtr)
+    {
+        IntPtr timelineInfoPtr;
+        FMOD.RESULT result = instance.getUserData(out timelineInfoPtr);
+        if (result != FMOD.RESULT.OK)
+        {
+            Debug.LogError("Timeline Callback error: " + result);
+        }
+        else if (timelineInfoPtr != IntPtr.Zero)
+        {
+            GCHandle timelineHandle = GCHandle.FromIntPtr(timelineInfoPtr);
+            TimelineInfo timelineInfo = (TimelineInfo)timelineHandle.Target;
+
+            switch (type)
+            {
+                case FMOD.Studio.EVENT_CALLBACK_TYPE.TIMELINE_BEAT:
+                    {
+                        var parameter = (FMOD.Studio.TIMELINE_BEAT_PROPERTIES)Marshal.PtrToStructure(parameterPtr, typeof(FMOD.Studio.TIMELINE_BEAT_PROPERTIES));
+                        timelineInfo.currentMusicBar = parameter.bar;
+                        bar = timelineInfo.currentMusicBar;
+                    }
+                    break;
+                case FMOD.Studio.EVENT_CALLBACK_TYPE.TIMELINE_MARKER:
+                    {
+                        var parameter = (FMOD.Studio.TIMELINE_MARKER_PROPERTIES)Marshal.PtrToStructure(parameterPtr, typeof(FMOD.Studio.TIMELINE_MARKER_PROPERTIES));
+                        timelineInfo.lastMarker = parameter.name;
+                        marker = timelineInfo.lastMarker;
+                    }
+                    break;
+            }
+        }
+        return FMOD.RESULT.OK;
+    }
+}
