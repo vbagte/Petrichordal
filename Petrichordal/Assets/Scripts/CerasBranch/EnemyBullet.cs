4d31d603a217d3f149fcfc160b658bca4566c6f3
CeraBee <64990619+CeraBee@users.noreply.github.com>
2020-06-02 16:14:21 -0700

    Revert "Revert "level 3/spaceneedle""
    This reverts commit 5ac9bed0457c32dde113b4be79bdd6a7178ecb8c.

--- Petrichordal/Assets/Scripts/CerasBranch/EnemyBullet.cs
+++ Petrichordal/Assets/Scripts/CerasBranch/EnemyBullet.cs
@@ -10,7 +10,7 @@ public class EnemyBullet : MonoBehaviour
     void Start()
     {
         //  Debug.Log(transform.rotation);
-        Destroy(gameObject, 10f);
+        //Destroy(gameObject, 10f);
 
     }
 
@@ -20,7 +20,7 @@ public class EnemyBullet : MonoBehaviour
       //  transform.up *= speed;
        // transform.Rotate(new Vector3(transform.rotation.x, transform.rotation.y, transform.rotation.z));
         transform.Translate(new Vector3(0, Time.deltaTime*speed, 0));
-        if (transform.position.y >= 5 || transform.position.x <=-9.5 || transform.position.y<=-5) Destroy(gameObject);
+        if (transform.position.y >= 5 || transform.position.x <=-9.5 || transform.position.y<=-5 || transform.position.x >=9.5) Destroy(gameObject);
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
--- Petrichordal/Assets/Scripts/CerasBranch/eventtrigger.cs
+++ Petrichordal/Assets/Scripts/CerasBranch/eventtrigger.cs
@@ -6,33 +6,38 @@ public class eventtrigger : MonoBehaviour
 {
     private long counter;
     public long seconds=8;
-    public int new_speed_multiplier = 1;
+    public float speed_on_exit_multiplier = 1;
+    public float new_speed_multiplier = 1;
         public GameObject foreground;
     public GameObject midground;
     public GameObject background;
+    private float bgOS, mgOS, fgOS;
     // Start is called before the first frame update
     void Start()
     {
         counter = 0;
+        bgOS = background.GetComponent<Scroll>().layerspeed;
+        mgOS = midground.GetComponent<Scroll>().layerspeed;
+        fgOS = foreground.GetComponent<Scroll>().layerspeed;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.x <= 10.5)
+        if (transform.position.x <= 10 && transform.position.x >= -10 && transform.position.y >= -5.5 && transform.position.y < 3.5)    
         {
             counter++;
             if (Mathf.Floor(counter / 60) < seconds)
             {
-               background.GetComponent<Scroll>().layerspeed = 0;
-                midground.GetComponent<Scroll>().layerspeed = 0;
-                foreground.GetComponent<Scroll>().layerspeed = 0;
+                background.GetComponent<Scroll>().layerspeed = bgOS * new_speed_multiplier;
+                midground.GetComponent<Scroll>().layerspeed = mgOS * new_speed_multiplier;
+                foreground.GetComponent<Scroll>().layerspeed = fgOS * new_speed_multiplier;
             }
             else
             {
-               background.GetComponent<Scroll>().layerspeed = 1*new_speed_multiplier;
-                midground.GetComponent<Scroll>().layerspeed = 2 * new_speed_multiplier;
-                foreground.GetComponent<Scroll>().layerspeed = 16 * new_speed_multiplier;
+               background.GetComponent<Scroll>().layerspeed = bgOS*speed_on_exit_multiplier;
+                midground.GetComponent<Scroll>().layerspeed = mgOS * speed_on_exit_multiplier;
+                foreground.GetComponent<Scroll>().layerspeed = fgOS * speed_on_exit_multiplier;
                 Destroy(gameObject);
             }
              
