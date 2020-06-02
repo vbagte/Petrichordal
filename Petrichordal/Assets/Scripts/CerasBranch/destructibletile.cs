6eba0afe52c12de1d0543af43322a8b8263ea615
rlandon96 <50156134+rlandon96@users.noreply.github.com>
2020-06-02 16:05:26 -0700

    Revert "Revert "Merge branch 'master' of https://github.com/rlandon96/Petrichordal""
    This reverts commit 161c73d70d745283f10ce2ad2319bad18b1f3d5b.

---
+++ Petrichordal/Assets/Scripts/CerasBranch/destructibletile.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class destructibletile : MonoBehaviour
+{
+  
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+              Tilemap tilemap = GetComponent<Tilemap>();
+        if (collision.gameObject.tag == "PlayerShot")
+        {
+        Vector3 hitPosition = new Vector3();
+            foreach (ContactPoint2D hit in collision.contacts)
+            {
+            hitPosition.x = hit.point.x;
+            hitPosition.y = hit.point.y;
+                tilemap.SetTile(tilemap.WorldToCell(hitPosition), null);
+            }
+            Destroy(collision.gameObject);
+        }
+    
+
+    }
+}
